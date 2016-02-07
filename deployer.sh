3b8612eecf73f352adb0f19c92604e5d33bf1cae
Alex Seif <alex.seif@gmail.com>
2016-02-08 00:44:02 +0200

    Removing assetic

--- deployer.sh
+++ deployer.sh
@@ -1,6 +1,5 @@
 git pull
 php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader
 php app/console cache:clear --env=prod --no-debug
-php app/console assetic:dump --env=prod --no-debug
 php app/console doctrine:schema:update --force --env=prod --no-debug
 php app/console assets:install --env=prod --no-debug
