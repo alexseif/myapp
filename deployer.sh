2870447fcbb64e3247667ca9e31b47b481a7b1e9
Alex Seif <alex.seif@gmail.com>
2016-04-15 06:33:09 +0200

    deployer for mt

--- deployer.sh
+++ deployer.sh
@@ -1,6 +1,8 @@
 export PATH="$PATH:/usr/bin"
 git pull
-php -d allow_url_fopen=1 composer.phar install --no-dev --optimize-autoloader
-php app/console cache:clear --env=prod --no-debug
-php app/console doctrine:schema:update --force --env=prod --no-debug
-php app/console assets:install --env=prod --no-debug
+export SYMFONY_ENV=prod
+php-latest -d allow_url_fopen=1 -d php_openssl=1 composer.phar install --no-dev --optimize-autoloader 
+php-latest app/console cache:clear --env=prod --no-debug
+php-latest app/console assetic:dump --env=prod --no-debug
+php-latest app/console doctrine:schema:update --force --env=prod 
+php-latest app/console assets:install --env=prod 
