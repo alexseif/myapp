549672ae0fa137461ecf284f6e3a74edc38a5ab3
Alex Seif <alex.seif@gmail.com>
2016-04-25 06:27:31 +0200

    Notes

---
+++ src/AppBundle/Controller/NotesController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use AppBundle\Entity\Notes;
+use AppBundle\Form\NotesType;
+
+/**
+ * Notes controller.
+ *
+ * @Route("/notes")
+ */
+class NotesController extends Controller
+{
+    /**
+     * Lists all Notes entities.
+     *
+     * @Route("/", name="notes_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $notes = $em->getRepository('AppBundle:Notes')->findAll();
+
+        return $this->render('notes/index.html.twig', array(
+            'notes' => $notes,
+        ));
+    }
+
+    /**
+     * Creates a new Notes entity.
+     *
+     * @Route("/new", name="notes_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $note = new Notes();
+        $form = $this->createForm('AppBundle\Form\NotesType', $note);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($note);
+            $em->flush();
+
+            return $this->redirectToRoute('notes_show', array('id' => $note->getId()));
+        }
+
+        return $this->render('notes/new.html.twig', array(
+            'note' => $note,
+            'notes_form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Notes entity.
+     *
+     * @Route("/{id}", name="notes_show")
+     * @Method("GET")
+     */
+    public function showAction(Notes $note)
+    {
+        $deleteForm = $this->createDeleteForm($note);
+
+        return $this->render('notes/show.html.twig', array(
+            'note' => $note,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Notes entity.
+     *
+     * @Route("/{id}/edit", name="notes_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Notes $note)
+    {
+        $deleteForm = $this->createDeleteForm($note);
+        $editForm = $this->createForm('AppBundle\Form\NotesType', $note);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($note);
+            $em->flush();
+
+            return $this->redirectToRoute('notes_edit', array('id' => $note->getId()));
+        }
+
+        return $this->render('notes/edit.html.twig', array(
+            'note' => $note,
+            'notes_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Notes entity.
+     *
+     * @Route("/{id}", name="notes_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Notes $note)
+    {
+        $form = $this->createDeleteForm($note);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($note);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('notes_index');
+    }
+
+    /**
+     * Creates a form to delete a Notes entity.
+     *
+     * @param Notes $note The Notes entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Notes $note)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('notes_delete', array('id' => $note->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/Notes.php
@@ -0,0 +1,129 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Notes
+ *
+ * @ORM\Table(name="notes")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NotesRepository")
+ */
+class Notes
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="note", type="text")
+     */
+    private $note;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+    
+    function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->type= 'general';
+    }
+
+        /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set note
+     *
+     * @param string $note
+     * @return Notes
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return string 
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     * @return Notes
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Notes
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
