29750bd4bf724d9d3057e75a11a0b697706fb10d
Alex Seif <alex.seif@gmail.com>
2016-07-23 16:10:44 +0200

    Some new UI features

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,51 +8,48 @@ use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Entity\Tasks;
 use AppBundle\Form\TasksType;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
 
-    /**
-     * @Route("/", name="dashboard")
-     */
-    public function dashboardAction(Request $request)
-    {
-        $em = $this->getDoctrine()->getManager();
+  /**
+   * @Route("/", name="dashboard")
+   */
+  public function dashboardAction(Request $request) {
+    $em = $this->getDoctrine()->getManager();
 
-        $taskLists = $em->getRepository('AppBundle:TaskLists')->findAll();
-        $tasks = $em->getRepository('AppBundle:Tasks')->findUnlisted();
-        $days = $em->getRepository('AppBundle:Days')->getActiveCards();
-        $accounts = $em->getRepository('AppBundle:Accounts')->findAll();
-        $projects = $em->getRepository('AppBundle:Projects')->findAll();
-        $txnDate = $em->getRepository('AppBundle:Transactions')->getFirstDate();
-        $txnAvg = $em->getRepository('AppBundle:Transactions')->getAvg();
+    $taskLists = $em->getRepository('AppBundle:TaskLists')->findAllWithActiveTasks();
+    $tasks = $em->getRepository('AppBundle:Tasks')->findUnlisted();
+    $days = $em->getRepository('AppBundle:Days')->getActiveCards();
+    $accounts = $em->getRepository('AppBundle:Accounts')->findAll();
+    $projects = $em->getRepository('AppBundle:Projects')->findAll();
+    $txnDate = $em->getRepository('AppBundle:Transactions')->getFirstDate();
+    $txnAvg = $em->getRepository('AppBundle:Transactions')->getAvg();
 
-        return $this->render('default/dashboard.html.twig', array(
-                    'taskLists' => $taskLists,
-                    'tasks' => $tasks,
-                    'days' => $days,
-                    'accounts' => $accounts,
-                    'projects' => $projects,
-                    'txnDate' => $txnDate,
-                    'txnAvg' => $txnAvg
-        ));
-    }
+    return $this->render('default/dashboard.html.twig', array(
+          'taskLists' => $taskLists,
+          'tasks' => $tasks,
+          'days' => $days,
+          'accounts' => $accounts,
+          'projects' => $projects,
+          'txnDate' => $txnDate,
+          'txnAvg' => $txnAvg
+    ));
+  }
 
-    /**
-     * @Route("/focus", name="focus")
-     */
-    public function focusAction()
-    {
-        $em = $this->getDoctrine()->getManager();
+  /**
+   * @Route("/focus", name="focus")
+   */
+  public function focusAction() {
+    $em = $this->getDoctrine()->getManager();
 
-        $tasks = $em->getRepository('AppBundle:Tasks')->focusList();
-        $task = new Tasks();
-        $form = $this->createForm(TasksType::class, $task, array(
-            'action' => $this->generateUrl('tasks_new')
-        ));
-        return $this->render('default/focus.html.twig', array(
-                    'tasks' => $tasks,
-                    'task_form' => $form->createView(),
-        ));
-    }
+    $tasks = $em->getRepository('AppBundle:Tasks')->focusList();
+    $task = new Tasks();
+    $form = $this->createForm(TasksType::class, $task, array(
+      'action' => $this->generateUrl('tasks_new')
+    ));
+    return $this->render('default/focus.html.twig', array(
+          'tasks' => $tasks,
+          'task_form' => $form->createView(),
+    ));
+  }
 
 }
--- web/js/tasks.js
+++ web/js/tasks.js
@@ -1,42 +1,44 @@
 $(document).ready(function () {
-    //Checking Tasks
-    $('.task-list input[type="checkbox"]').change(function () {
-        taskId = $(this).data('taskid');
-        if ($(this).is(":checked")) {
-            $(this).parent().parent().addClass('completed');
-            completed = 1;
-        } else {
-            $(this).parent().parent().removeClass('completed');
-            completed = 0;
-        }
-        $.ajax({
-            type: "POST",
-            url: tasks_path + taskId + "/edit",
-            dataType: "json",
-            data: {
-                "completed": completed
-            }
-        });
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover({html: true, trigger: 'focus'});
+  //Checking Tasks
+  $('.task-list input[type="checkbox"]').change(function () {
+    taskId = $(this).data('taskid');
+    if ($(this).is(":checked")) {
+      $(this).parent().parent().addClass('completed');
+      completed = 1;
+    } else {
+      $(this).parent().parent().removeClass('completed');
+      completed = 0;
+    }
+    $.ajax({
+      type: "POST",
+      url: tasks_path + taskId + "/edit",
+      dataType: "json",
+      data: {
+        "completed": completed
+      }
     });
+  });
 
-    //Sorting Tasks
-    $(".task-list").sortable({
-        placeholder: "ui-state-highlight",
-        items: "li:not(.completed)",
-        update: function (event, ui) {
-            var data = $(this).sortable("serialize", {"key": "tasks[][id]", attribute: "data-order"});
-            console.log(data);
-            $.ajax({
-                data: data,
-                dataType: "json",
-                type: 'POST',
-                url: tasks_order
-            });
-        }
-    });
+  //Sorting Tasks
+  $(".task-list").sortable({
+    placeholder: "ui-state-highlight",
+    items: "li:not(.completed)",
+    update: function (event, ui) {
+      var data = $(this).sortable("serialize", {"key": "tasks[][id]", attribute: "data-order"});
+      console.log(data);
+      $.ajax({
+        data: data,
+        dataType: "json",
+        type: 'POST',
+        url: tasks_order
+      });
+    }
+  });
 
-    //Modal handling
-    $('#newTask').on('shown.bs.modal', function () {
-        $('#tasks_task').focus();
-    })
+  //Modal handling
+  $('#newTask').on('shown.bs.modal', function () {
+    $('#tasks_task').focus();
+  });
 });
