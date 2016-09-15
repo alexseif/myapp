16c82e6b4f293e3ba6ecf6d0d5add1e603256bd5
Alex Seif <alex.seif@gmail.com>
2016-09-16 00:19:40 +0200

    delete backups automatically, keep last 5

--- src/AppBundle/Controller/BackupsController.php
+++ src/AppBundle/Controller/BackupsController.php
@@ -10,6 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 
 /**
  * Backups controller.
@@ -19,63 +21,84 @@ use Symfony\Component\Console\Output\BufferedOutput;
 class BackupsController extends Controller
 {
 
-    /**
-     * @Route("/", name="backups")
-     * 
-     */
-    public function indexAction()
-    {
-        $finder = new Finder();
+  /**
+   * @Route("/", name="backups")
+   * 
+   */
+  public function indexAction()
+  {
+    $finder = new Finder();
 
+    try {
+      $finder->files()->in($this->get('kernel')->getRootDir() . '/../backups')->sortByModifiedTime();
+    } catch (\Exception $exc) {
+      $finder = null;
+    }
+
+    return $this->render('backups/index.html.twig', array(
+          'finder' => $finder
+    ));
+  }
+
+  /**
+   * @Route("/generate", name="backups_generate")
+   */
+  public function generateAction()
+  {
+    $finder = new Finder();
+
+    try {
+      $finder->files()->in($this->get('kernel')->getRootDir() . '/../backups')->sortByModifiedTime();
+    } catch (\Exception $exc) {
+      $finder = null;
+    }
+
+    $fs = new Filesystem();
+    $count = $finder->count();
+    foreach ($finder->files() as $file) {
+      if ($count >= 5) {
         try {
-            $finder->files()->in($this->get('kernel')->getRootDir() . '/../backups')->sortByModifiedTime();
-        } catch (\Exception $exc) {
-            $finder = null;
+          $fs->remove($file);
+        } catch (IOExceptionInterface $e) {
+          echo "An error occurred while creating your directory at " . $e->getPath();
         }
-
-        return $this->render('backups/index.html.twig', array(
-                    'finder' => $finder
-        ));
+        $count--;
+      }
     }
 
-    /**
-     * @Route("/generate", name="backups_generate")
-     */
-    public function generateAction()
-    {
-        $kernel = $this->get('kernel');
-        $application = new Application($kernel);
-        $application->setAutoExit(false);
+    $kernel = $this->get('kernel');
+    $application = new Application($kernel);
+    $application->setAutoExit(false);
 
-        $input = new ArrayInput(array(
-            'command' => 'zenstruck:backup:run',
-            'profile' => 'daily',
-            '-e' => 'prod',
-            '--clear',
-        ));
-        $output = new BufferedOutput();
-        $application->run($input, $output);
-        $content = $output->fetch();
+    $input = new ArrayInput(array(
+      'command' => 'zenstruck:backup:run',
+      'profile' => 'daily',
+      '-e' => 'prod',
+      '--clear',
+    ));
+    $output = new BufferedOutput();
+    $application->run($input, $output);
+    $content = $output->fetch();
 
-        $this->addFlash('success', 'Backup generated');
-        return $this->redirectToRoute('backups');
-    }
+    $this->addFlash('success', 'Backup generated');
+    return $this->redirectToRoute('backups');
+  }
 
-    /**
-     * @Route("/{file}", name="backups_download")
-     */
-    public function downloadAction($file)
-    {
-        $content = file_get_contents($this->get('kernel')->getRootDir() . '/../backups/' . $file);
+  /**
+   * @Route("/{file}", name="backups_download")
+   */
+  public function downloadAction($file)
+  {
+    $content = file_get_contents($this->get('kernel')->getRootDir() . '/../backups/' . $file);
 
-        $response = new Response();
+    $response = new Response();
 
-        //set headers
-        $response->headers->set('Content-Type', 'application/gzip');
-        $response->headers->set('Content-Disposition', 'attachment;filename="' . $file);
+    //set headers
+    $response->headers->set('Content-Type', 'application/gzip');
+    $response->headers->set('Content-Disposition', 'attachment;filename="' . $file);
 
-        $response->setContent($content);
-        return $response;
-    }
+    $response->setContent($content);
+    return $response;
+  }
 
 }
