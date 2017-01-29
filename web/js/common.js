d0973f78d45c57d33482dfee167979307cce19dc
Alex Seif <alex.seif@gmail.com>
2017-01-29 22:11:28 +0200

    Confirm on delete and disable focus js till I fix bug

--- web/js/common.js
+++ web/js/common.js
@@ -7,4 +7,8 @@ $(document).ready(function () {
     dateFormat: $('.datepicker').data('date-format')
   });
   $('form :input').first().focus();
+  
+  $('.btn-delete').click(function () {
+    return confirm('Are you sure you want to delete?');
+  });
 });
--- web/js/tasks.js
+++ web/js/tasks.js
@@ -92,6 +92,5 @@ function focusInit() {
       update: updateOrder
     });
   }
-  drawFocusTasks();
-  focusTitle();
+//  drawFocusTasks();
 }
