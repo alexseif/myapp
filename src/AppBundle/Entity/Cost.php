7302c4f4f9434cfe2369ee64d20af0affec74afb
Alex Seif <alex.seif@gmail.com>
2017-02-08 01:15:37 +0200

    Cost

---
+++ src/AppBundle/Entity/Cost.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Cost
+ *
+ * @ORM\Table(name="cost")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CostRepository")
+ */
+class Cost
+{
+
+  /**
+   * @var int
+   *
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  private $id;
+
+  /**
+   * @var string
+   *
+   * @ORM\Column(name="name", type="string", length=255)
+   */
+  private $name;
+
+  /**
+   * @var float
+   *
+   * @ORM\Column(name="value", type="float")
+   */
+  private $value;
+
+  /**
+   * @ORM\ManyToOne(targetEntity="Currency")
+   * @ORM\JoinColumn(name="currency_id", referencedColumnName="id")
+   */
+  private $currency;
+
+  /**
+   * @var \DateTime
+   *
+   * @ORM\Column(name="generatedAt", type="datetime")
+   */
+  private $generatedAt;
+
+  /**
+   * @var \DateTime
+   *
+   * @ORM\Column(name="createdAt", type="datetime")
+   */
+  private $createdAt;
+
+  public function __construct()
+  {
+    $this->generatedAt = $this->createdAt = new \DateTime();
+  }
+
+  /**
+   * Get id
+   *
+   * @return int
+   */
+  public function getId()
+  {
+    return $this->id;
+  }
+
+  /**
+   * Set name
+   *
+   * @param string $name
+   *
+   * @return Cost
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+
+    return $this;
+  }
+
+  /**
+   * Get name
+   *
+   * @return string
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  /**
+   * Set value
+   *
+   * @param float $value
+   *
+   * @return Cost
+   */
+  public function setValue($value)
+  {
+    $this->value = $value;
+
+    return $this;
+  }
+
+  /**
+   * Get value
+   *
+   * @return float
+   */
+  public function getValue()
+  {
+    return $this->value;
+  }
+
+  /**
+   * Set currency
+   *
+   * @param integer $currency
+   *
+   * @return Cost
+   */
+  public function setCurrency($currency)
+  {
+    $this->currency = $currency;
+
+    return $this;
+  }
+
+  /**
+   * Get currency
+   *
+   * @return int
+   */
+  public function getCurrency()
+  {
+    return $this->currency;
+  }
+
+  /**
+   * Set generatedAt
+   *
+   * @param \DateTime $generatedAt
+   *
+   * @return Cost
+   */
+  public function setGeneratedAt($generatedAt)
+  {
+    $this->generatedAt = $generatedAt;
+
+    return $this;
+  }
+
+  /**
+   * Get generatedAt
+   *
+   * @return \DateTime
+   */
+  public function getGeneratedAt()
+  {
+    return $this->generatedAt;
+  }
+
+  /**
+   * Set createdAt
+   *
+   * @param \DateTime $createdAt
+   *
+   * @return Cost
+   */
+  public function setCreatedAt($createdAt)
+  {
+    $this->createdAt = $createdAt;
+
+    return $this;
+  }
+
+  /**
+   * Get createdAt
+   *
+   * @return \DateTime
+   */
+  public function getCreatedAt()
+  {
+    return $this->createdAt;
+  }
+
+}
