84c39af5472910b50b6834894a589fc2c8f95647
Alex Seif <alex.seif@gmail.com>
2017-03-08 01:49:38 +0200

    TaskList Tasks count of completed and not completed

--- src/AppBundle/Entity/TaskLists.php
+++ src/AppBundle/Entity/TaskLists.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Criteria;
 
 /**
  * TaskLists
@@ -173,9 +174,14 @@ class TaskLists
    *
    * @return \Doctrine\Common\Collections\Collection 
    */
-  public function getTasks()
+  public function getTasks($showComplete = true)
   {
-    return $this->tasks;
+    $criteria = Criteria::create();
+    if ($showComplete !== true) {
+      $criteria->where(Criteria::expr()->eq('completed', false));
+    }
+    $criteria->orderBy(array('completed' => 'asc'));
+    return $this->tasks->matching($criteria);
   }
 
   public function getEstTotal()
