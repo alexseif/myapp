e50d40d7212327d961cfe492b555ec707703f49f
Alex Seif <alex.seif@gmail.com>
2017-06-12 16:58:40 +0200

    Ranting page

--- src/AppBundle/Controller/RantingController.php
+++ src/AppBundle/Controller/RantingController.php
@@ -5,22 +5,84 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Ranting controller.
  *
- * @Route("/ranting")
+ * @Route("/ranting",)
  */
 class RantingController extends Controller
 {
 
   /**
-   * @Route("/")
-   * @Template()
+   * @Route("/", name="ranting_show")
+   * @Template("ranting/index.html.twig")
    */
-  public function indexAction()
+  public function showAction()
   {
-    
+    return array();
+  }
+
+  /**
+   * @Route("/edit", name="ranting_edit")
+   * @Template("ranting/edit.html.twig")
+   */
+  public function editAction(Request $request)
+  {
+    // Output file.
+    $filepath = $this->container->getParameter('kernel.root_dir') .
+        '/Resources/views/ranting/ranting.html.twig';
+    // Dummy object used in the form.
+    $file = new \ArrayObject();
+
+    // Check that the output file exists.
+    if (file_exists($filepath)) {
+      // Load the file content.
+      $file->content = file_get_contents($filepath);
+    } else {
+      //TODO: revise
+      // The file is missing, load the file located in the Bundle instead.
+      $file->content = file_get_contents(
+          $this->container->get('kernel')->locateResource(
+              '/Resources/views/ranting/menu.html.twig')
+      );
+    }
+
+    // Declare the form.
+    $form = $this->createFormBuilder($file)
+        // A textarea for the file content.
+        ->add('content', 'textarea', array(
+          'attr' => array(
+            'rows' => 16,
+            'class' => 'tinymce',
+          )
+        ))
+        ->add('submit', 'submit', array(
+          'label' => 'Save'
+        ))
+        ->getForm();
+
+    $form->handleRequest($request);
+    if ($form->isValid()) {
+      // Save the file in app/Resources/...
+      file_put_contents($filepath, $file->content);
+
+      // Clear cache (otherwise the changes won't be applied)
+      // Be patient, it can take about 15 seconds.
+      $console = 'php ' . $this->container->getParameter('kernel.root_dir') .
+          '/console';
+
+//      exec($console . ' cache:clear --env=prod');
+//      exec($console . ' cache:warmup --env=prod');
+
+      $this->get('session')->getFlashBag()->add('notice', 'Content saved succesfully.');
+
+      return $this->redirect($this->generateUrl('ranting_show'));
+    }
+    return array(
+      'form' => $form->createView(),
+    );
   }
 
 }
