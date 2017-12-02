303b9c4530339cc18c9701031890a73b488a034f
Alex Seif <alex.seif@gmail.com>
2017-12-02 22:35:18 +0200

    Removing Planner Feature

---
+++ app/DoctrineMigrations/Version20171202203432.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171202203432 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE planner_tasks DROP FOREIGN KEY FK_DA537BF75346EAE1');
+        $this->addSql('DROP TABLE planner');
+        $this->addSql('DROP TABLE planner_tasks');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE planner (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, due DATETIME NOT NULL, UNIQUE INDEX UNIQ_955177615E237E06 (name), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE planner_tasks (planner_id INT NOT NULL, task_id INT NOT NULL, UNIQUE INDEX UNIQ_DA537BF78DB60186 (task_id), INDEX IDX_DA537BF75346EAE1 (planner_id), PRIMARY KEY(planner_id, task_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE planner_tasks ADD CONSTRAINT FK_DA537BF75346EAE1 FOREIGN KEY (planner_id) REFERENCES planner (id)');
+        $this->addSql('ALTER TABLE planner_tasks ADD CONSTRAINT FK_DA537BF78DB60186 FOREIGN KEY (task_id) REFERENCES tasks (id)');
+    }
+}
