d21aa2672dbd4ccfc8dcf3de24c8f995ae374dca
Alex Seif <alex.seif@gmail.com>
2018-04-10 21:23:48 +0200

    sh to sync branches

---
+++ sync-branches.sh
@@ -0,0 +1,15 @@
+git checkout beta
+git merge master
+ggp
+git checkout budgeting
+git merge master
+ggp
+git checkout focus
+git merge master
+ggp
+git checkout google-api
+git merge master
+ggp
+git checkout symfony_upgrade
+git merge master
+ggp
