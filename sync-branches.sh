367f1741d1818a798df4f8c8bf6e7d4db5b6e185
Alex Seif <alex.seif@gmail.com>
2018-07-27 17:07:54 +0200

    pull before merge in sync branches

--- sync-branches.sh
+++ sync-branches.sh
@@ -1,21 +1,28 @@
 git checkout beta
+git pull --all
 git merge master
 git push origin beta
 git checkout budgeting
+git pull --all
 git merge master
 git push origin budgeting
 git checkout focus
+git pull --all
 git merge master
 git push origin focus
 git checkout google-api
+git pull --all
 git merge master
 git push origin google-api
 git checkout symfony_upgrade
+git pull --all
 git merge master
 git push origin symfony_upgrade
 git checkout refactor
+git pull --all
 git merge master
 git push origin refactor
 git checkout client-feature
+git pull --all
 git merge master
 git push origin client-feature
