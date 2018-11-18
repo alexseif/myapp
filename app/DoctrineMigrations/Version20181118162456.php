d26272d34e2d965d53d3904a15845d92d413d48a
Alex Seif <alex.seif@gmail.com>
2018-11-18 16:32:49 +0200

    Balance Crud

---
+++ app/DoctrineMigrations/Version20181118162456.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20181118162456 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE balance CHANGE item title VARCHAR(255) NOT NULL, CHANGE balancedate balanceAt DATE NOT NULL, CHANGE balance amount INT NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE balance CHANGE title item VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE balanceat balanceDate DATE NOT NULL, CHANGE amount balance INT NOT NULL');
+    }
+}
---
+++ src/AppBundle/Form/BalanceType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+
+class BalanceType extends AbstractType
+{
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+        ->add('title')
+        ->add('balanceAt', DateType::class, array(
+          'widget' => 'single_text',
+          'format' => 'yyyy-MM-dd',
+        ))
+        ->add('amount', MoneyType::class)
+    ;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $resolver->setDefaults(array(
+      'data_class' => 'AppBundle\Entity\Balance'
+    ));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getBlockPrefix()
+  {
+    return 'appbundle_balance';
+  }
+
+}
