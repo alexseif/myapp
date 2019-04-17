583e9b3493c3d211a5db3c41e881a8518e4ac578
Alex Seif <alex.seif@gmail.com>
2019-04-17 20:23:32 +0200

    mediatemple composer update

--- deployer.sh
+++ deployer.sh
@@ -1,7 +1,7 @@
 export PATH="$PATH:/usr/bin"
 git pull
 export SYMFONY_ENV=prod
-php-latest -d allow_url_fopen=1 -d php_openssl=1 composer.phar install --no-dev --optimize-autoloader 
+php-latest -d allow_url_fopen=1 -d php_openssl=1 -d open_basedir=NULL composer.phar install --no-dev --optimize-autoloader 
 php-latest bin/console cache:clear --env=prod --no-debug
 php-latest bin/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod 
 php-latest bin/console assets:install --env=prod 
