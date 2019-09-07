a05d074a7b432abfcd1711cd96678d6214917054
Alex Seif <alex.seif@gmail.com>
2019-09-07 19:02:31 +0200

    Thing CRUD

---
+++ src/AppBundle/Controller/ThingController.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Thing;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Thing controller.
+ *
+ * @Route("thing")
+ */
+class ThingController extends Controller
+{
+
+  /**
+   * Lists all thing entities.
+   *
+   * @Route("/", name="thing_index")
+   * @Method("GET")
+   */
+  public function indexAction()
+  {
+    $em = $this->getDoctrine()->getManager();
+
+    $things = $em->getRepository('AppBundle:Thing')->findAll();
+
+    return $this->render('thing/index.html.twig', array(
+          'things' => $things,
+    ));
+  }
+
+  /**
+   * Creates a new thing entity.
+   *
+   * @Route("/new", name="thing_new")
+   * @Method({"GET", "POST"})
+   */
+  public function newAction(Request $request)
+  {
+    $thing = new Thing();
+    $form = $this->createForm('AppBundle\Form\ThingType', $thing);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      $em->persist($thing);
+      $em->flush();
+
+      return $this->redirectToRoute('thing_show', array('id' => $thing->getId()));
+    }
+
+    return $this->render('thing/new.html.twig', array(
+          'thing' => $thing,
+          'form' => $form->createView(),
+    ));
+  }
+
+  /**
+   * Finds and displays a thing entity.
+   *
+   * @Route("/{id}", name="thing_show")
+   * @Method("GET")
+   */
+  public function showAction(Thing $thing)
+  {
+    $deleteForm = $this->createDeleteForm($thing);
+
+    return $this->render('thing/show.html.twig', array(
+          'thing' => $thing,
+          'delete_form' => $deleteForm->createView(),
+    ));
+  }
+
+  /**
+   * Displays a form to edit an existing thing entity.
+   *
+   * @Route("/{id}/edit", name="thing_edit")
+   * @Method({"GET", "POST"})
+   */
+  public function editAction(Request $request, Thing $thing)
+  {
+    $deleteForm = $this->createDeleteForm($thing);
+    $editForm = $this->createForm('AppBundle\Form\ThingType', $thing);
+    $editForm->handleRequest($request);
+
+    if ($editForm->isSubmitted() && $editForm->isValid()) {
+      $this->getDoctrine()->getManager()->flush();
+
+      return $this->redirectToRoute('thing_edit', array('id' => $thing->getId()));
+    }
+
+    return $this->render('thing/edit.html.twig', array(
+          'thing' => $thing,
+          'edit_form' => $editForm->createView(),
+          'delete_form' => $deleteForm->createView(),
+    ));
+  }
+
+  /**
+   * Deletes a thing entity.
+   *
+   * @Route("/{id}", name="thing_delete")
+   * @Method("DELETE")
+   */
+  public function deleteAction(Request $request, Thing $thing)
+  {
+    $form = $this->createDeleteForm($thing);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $em = $this->getDoctrine()->getManager();
+      $em->remove($thing);
+      $em->flush();
+    }
+
+    return $this->redirectToRoute('thing_index');
+  }
+
+  /**
+   * Creates a form to delete a thing entity.
+   *
+   * @param Thing $thing The thing entity
+   *
+   * @return \Symfony\Component\Form\Form The form
+   */
+  private function createDeleteForm(Thing $thing)
+  {
+    return $this->createFormBuilder()
+            ->setAction($this->generateUrl('thing_delete', array('id' => $thing->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+    ;
+  }
+
+}
