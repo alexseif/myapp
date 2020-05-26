14a838025dff1be083c596161a09f77bde90aa3b
Alex Seif <alex.seif@gmail.com>
2020-05-26 21:28:54 +0200

    Holiday entity

---
+++ src/AppBundle/Form/HolidayType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Holiday;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HolidayType extends AbstractType
+{
+
+  public function buildForm(FormBuilderInterface $builder, array $options)
+  {
+    $builder
+        ->add('name')
+        ->add('date')
+    ;
+  }
+
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $resolver->setDefaults([
+      'data_class' => Holiday::class,
+    ]);
+  }
+
+}
