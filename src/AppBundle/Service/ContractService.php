d88d2e685e2fefc47f236468ebdef5d4219bd299
Alex Seif <alex.seif@gmail.com>
2020-06-13 14:40:57 +0200

    Contract progress by the 25th

---
+++ src/AppBundle/Service/ContractService.php
@@ -0,0 +1,73 @@
+<?php
+
+/*
+ * The following content was designed & implemented under AlexSeif.com
+ */
+
+namespace AppBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+
+/**
+ * Description of Contract Service
+ *
+ * @author Alex Seif <me@alexseif.com>
+ */
+class ContractService
+{
+
+  /**
+   * 
+   * @var EntityManager $em
+   */
+  protected $em;
+
+  /**
+   * 
+   * @var CostService $cs
+   */
+  protected $cs;
+
+  public function __construct(EntityManager $em)
+  {
+    $this->em = $em;
+  }
+
+  /**
+   * 
+   * @return array progress of contracts
+   */
+  public function progress()
+  {
+    $contracts = $this->em->getRepository('AppBundle:Contract')->findAll();
+    foreach ($contracts as $contract) {
+      $completedByClientThisMonth = $this->em->getRepository('AppBundle:Tasks')->findDurationCompletedByClientThisMonth($contract->getClient())['duration'];
+      $monthStart = new \DateTime();
+      $monthStart->setTime(00, 00, 00);
+      $monthStart->setDate($monthStart->format("Y"), $monthStart->format("m"), $monthStart->format("t"));
+      $monthStart->modify("-5 days");
+      if ($contract->getStartedAt() > $monthStart) {
+        $monthStart->setDate($contract->getStartedAt()->format('Y'), $contract->getStartedAt()->format('m'), $contract->getStartedAt()->format('d'));
+      }
+      $monthEnd = new \DateTime();
+      $monthEnd->setDate($monthEnd->format('Y'), $monthEnd->format('m'), $monthEnd->format('t'));
+      $monthEnd->modify("-5 days");
+      $monthEnd->setTime(23, 59, 59);
+      $workingDaysSoFar = \AppBundle\Util\DateRanges::numberOfWorkingDays($monthStart, new \DateTime());
+      $workingDaysLeft = \AppBundle\Util\DateRanges::numberOfWorkingDays(new \DateTime(), $monthEnd);
+      $expctedByClientThisMonth = $contract->getHoursPerDay() * $workingDaysSoFar * 60;
+      $duration = $this->em->getRepository('AppBundle:Tasks')->findCompletedByClientToday($contract->getClient())['duration'];
+      $difference = $expctedByClientThisMonth - $completedByClientThisMonth;
+      $workingDaysLeft = ($workingDaysLeft) ?: 1;
+      $overMonth = $difference / $workingDaysLeft;
+      $minutesPerDay = ($contract->getHoursPerDay() * 60) + $overMonth;
+      if ($difference < 0) {
+        $minutesPerDay = ($contract->getHoursPerDay() * 60);
+      }
+      $contract->percentage = $duration / $minutesPerDay * 100;
+      $contract->setName($contract->getName() . " " . floor($minutesPerDay / 60) . ":" . ($minutesPerDay % 60) . " || " . floor($difference / 60) . ":" . ($difference % 60));
+    }
+    return $contracts;
+  }
+
+}
