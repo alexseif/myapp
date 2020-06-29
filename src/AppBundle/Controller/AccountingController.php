bb7084e63eb5c15c5b7b3822ef6f0e89969f8936
Alex Seif <alex.seif@gmail.com>
2020-06-29 19:02:25 +0200

    Some refactoring

--- src/AppBundle/Controller/AccountingController.php
+++ src/AppBundle/Controller/AccountingController.php
@@ -95,67 +95,11 @@ class AccountingController extends Controller
     ));
   }
 
-  /**
-   * @Route("/statements", name="accounting_statements_page", methods={"GET"})
-   */
-  public function statementAction(Request $request)
-  {
-    $accountingFilterForm = $this->getFilterFunction($request);
-
-    $accountingFilterForm->handleRequest($request);
-
-    if ($accountingFilterForm->isSubmitted() && $accountingFilterForm->isValid()) {
-      // TODO: create statements
-      $accountingFilterData = $accountingFilterForm->getData();
-      $account = $accountingFilterData['account'];
-
-      $em = $this->getDoctrine()->getManager();
-      $txnRepo = $em->getRepository('AppBundle:AccountTransactions');
-      $txnPeriod = $txnRepo->queryAccountRange($account);
-
-      $begin = new \DateTime($txnPeriod['rangeStart']);
-      $begin->setDate($begin->format("Y"), $begin->format("m"), 1);
-      $end = new \DateTime($txnPeriod['rangeEnd']);
-      $interval = \DateInterval::createFromDateString('1 month');
-      $period = new \DatePeriod($begin, $interval, $end);
-      $monthsArray = array();
-
-      foreach ($period as $dt) {
-        $monthsArray[] = $dt;
-      }
-    }
-
-    return $this->render('AppBundle:Accounting:account.html.twig', array(
-          'accounting_filter_form' => $accountingFilterForm->createView(),
-          'account' => $account,
-          'txnPeriod' => $txnPeriod,
-          'monthsArray' => $monthsArray
-    ));
-  }
-
   protected function getFilterFunction(Request $request)
   {
     return $this->createForm(AccountingMainFilterType::class, $request->get('accounting_filter'), array(
           'method' => 'GET',
-//          'action' => $this->generateUrl('accounting_account_page')
     ));
-  }
-
-  public function nb_mois($date1, $date2)
-  {
-    $begin = new \DateTime($date1);
-    $end = new \DateTime($date2);
-    $end = $end->modify('+1 month');
-
-    $interval = \DateInterval::createFromDateString('1 month');
-
-    $period = new \DatePeriod($begin, $interval, $end);
-    $counter = 0;
-    foreach ($period as $dt) {
-      $counter++;
-    }
-
-    return $counter;
   }
 
 }
