00fbf22b8437995c2be2e0370b2226e2a009aa37
Alex Seif <alex.seif@gmail.com>
2020-12-04 17:43:23 +0200

    Day Manager init

---
+++ src/AppBundle/Service/DayManager.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * The following content was designed & implemented under AlexSeif.com
+ */
+
+namespace AppBundle\Service;
+
+/**
+ * Description of DayManager
+ *
+ * @author Alex Seif <me@alexseif.com>
+ */
+class DayManager
+{
+
+  public function whatsToday()
+  {
+    $today = new \DateTime();
+    return 'Today is ' . $today->format('l') . ' the ' . $today->format('jS') . ' of ' . $today->format('F') . ' and it\'s awesome.';
+  }
+
+  public function isWorkDay()
+  {
+    $today = new \DateTime();
+    if (in_array($today->format('N'), [5, 6])) { // 5 and 6 are weekend days
+      return false;
+    }
+    return true;
+  }
+
+  public function isWorkingDay()
+  {
+    if ($this->isWorkDay()) {
+      return 'Yes, it\'s a working day.';
+    } else {
+      return 'No, it\'s not a working day.';
+    }
+  }
+
+}
