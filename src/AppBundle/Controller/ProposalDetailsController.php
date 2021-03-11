f69570ce047513b355c024acf863f2451f0b7992
Alex Seif <alex.seif@gmail.com>
2021-03-11 12:34:17 +0200

    [wip] proposal details crud

--- src/AppBundle/Controller/ProposalDetailsController.php
+++ src/AppBundle/Controller/ProposalDetailsController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Proposal;
 use AppBundle\Entity\ProposalDetails;
 use AppBundle\Form\ProposalDetailsType;
 use AppBundle\Repository\ProposalDetailsRepository;
@@ -11,16 +12,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/proposal/details")
+ * @Route("/proposal/{proposal}/details")
  */
 class ProposalDetailsController extends Controller
 {
     /**
      * @Route("/", name="proposal_details_index", methods={"GET"})
      */
-    public function index(ProposalDetailsRepository $proposalDetailsRepository): Response
+    public function index(ProposalDetailsRepository $proposalDetailsRepository, Proposal $proposal): Response
     {
         return $this->render('proposal_details/index.html.twig', [
+            'proposal' => $proposal,
             'proposal_details' => $proposalDetailsRepository->findAll(),
         ]);
     }
@@ -28,7 +30,7 @@ class ProposalDetailsController extends Controller
     /**
      * @Route("/new", name="proposal_details_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, Proposal $proposal): Response
     {
         $proposalDetail = new ProposalDetails();
         $form = $this->createForm(ProposalDetailsType::class, $proposalDetail);
@@ -43,6 +45,7 @@ class ProposalDetailsController extends Controller
         }
 
         return $this->render('proposal_details/new.html.twig', [
+            'proposal' => $proposal,
             'proposal_detail' => $proposalDetail,
             'form' => $form->createView(),
         ]);
@@ -51,9 +54,10 @@ class ProposalDetailsController extends Controller
     /**
      * @Route("/{id}", name="proposal_details_show", methods={"GET"})
      */
-    public function show(ProposalDetails $proposalDetail): Response
+    public function show(Proposal $proposal, ProposalDetails $proposalDetail): Response
     {
         return $this->render('proposal_details/show.html.twig', [
+            'proposal' => $proposal,
             'proposal_detail' => $proposalDetail,
         ]);
     }
@@ -61,7 +65,7 @@ class ProposalDetailsController extends Controller
     /**
      * @Route("/{id}/edit", name="proposal_details_edit", methods={"GET","POST"})
      */
-    public function edit(Request $request, ProposalDetails $proposalDetail): Response
+    public function edit(Request $request, Proposal $proposal, ProposalDetails $proposalDetail): Response
     {
         $form = $this->createForm(ProposalDetailsType::class, $proposalDetail);
         $form->handleRequest($request);
@@ -69,10 +73,11 @@ class ProposalDetailsController extends Controller
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('proposal_details_index');
+            return $this->redirectToRoute('proposal_details_index', ['proposal' => $proposal]);
         }
 
         return $this->render('proposal_details/edit.html.twig', [
+            'proposal' => $proposal,
             'proposal_detail' => $proposalDetail,
             'form' => $form->createView(),
         ]);
@@ -81,14 +86,14 @@ class ProposalDetailsController extends Controller
     /**
      * @Route("/{id}", name="proposal_details_delete", methods={"DELETE"})
      */
-    public function delete(Request $request, ProposalDetails $proposalDetail): Response
+    public function delete(Request $request, Proposal $proposal, ProposalDetails $proposalDetail): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$proposalDetail->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $proposalDetail->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($proposalDetail);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('proposal_details_index');
+        return $this->redirectToRoute('proposal_details_index', ['proposal' => $proposal]);
     }
 }
