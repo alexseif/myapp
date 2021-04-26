74296e0b129c0ce32b74c7228278b6feb7645139
Alex Seif <alex.seif@gmail.com>
2021-04-26 07:52:31 +0200

    Workarea sortable

--- web/js/inbox.js
+++ web/js/inbox.js
@@ -1,33 +1,51 @@
 /* 
  * The following content was designed & implemented under AlexSeif.com
  */
-
+// @todo: move to tasks.js
 var inbox = {
-  init: function () {
-    $('#inbox .nav-link').click(this.loadTasks);
-    $('#inbox .nav-link').first().click();
-  },
-  loadTasks: function () {
-    var inboxTaskList = this;
-    var dataUrl = $(this).data('url');
-    var dataTarget = $(this).data('target');
-    $('#inbox .nav-item').removeClass('active-trail');
-    $(this).parent('.nav-item').addClass('active-trail');
-    //@TODO: Error handling 
-    var jqxhr = $.ajax({
-      url: dataUrl
-    })
+    init: function () {
+        $('#inbox .nav-link').click(this.loadTasks);
+        $('#inbox .nav-link').first().click();
+    },
+    loadTasks: function () {
+        var inboxTaskList = this;
+        var dataUrl = $(this).data('url');
+        var dataTarget = $(this).data('target');
+        $('#inbox .nav-item').removeClass('active-trail');
+        $(this).parent('.nav-item').addClass('active-trail');
+        //@TODO: Error handling
+        var jqxhr = $.ajax({
+            url: dataUrl
+        })
             .done(function (html) {
-              $(dataTarget).html(html);
-              Tasks.bindEvents();
+                $(dataTarget).html(html);
+                Tasks.init();
+                // Tasks.bindEvents();
+                $(".task-list").sortable({
+                    connectWith: ".task-list",
+                    items: ".task-item:not(.completed)",
+                    update: function () {
+                        var dataString = "";
+                        $('.task-list .task-item:not(.completed)').each(function () {
+                            dataString += "tasks[][id]=" + $(this).data('id') + "&";
+                        });
+                        $.ajax({
+                            data: dataString,
+                            dataType: "json",
+                            type: 'POST',
+                            url: tasks_order
+                        }).done(function () {
+                        });
+                    }
+                });
             })
             .fail(function () {
-              alert("error");
+                alert("error");
             })
             .always(function () {
 //              alert("complete");
             });
-    $('title').text($(this).text().trim() + " | myApp");
-  },
+        $('title').text($(this).text().trim() + " | myApp");
+    },
 }
 inbox.init();
