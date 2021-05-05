53441155a79edb65e527cca2865b1c8c158a5950
Alex Seif <alex.seif@gmail.com>
2021-05-05 19:22:44 +0200

    Recurr doesn't exist on production

--- app/DoctrineMigrations/Version20210505171313.php
+++ app/DoctrineMigrations/Version20210505171313.php
@@ -15,7 +15,6 @@ final class Version20210505171313 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DROP TABLE recurr');
         $this->addSql('DROP TABLE service');
     }
 
@@ -24,7 +23,6 @@ final class Version20210505171313 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE recurr (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, date_start DATE DEFAULT NULL, date_until DATE DEFAULT NULL, count INT DEFAULT NULL, freq_interval INT DEFAULT NULL, freq INT DEFAULT NULL, by_day LONGTEXT CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_unicode_ci` COMMENT \'(DC2Type:simple_array)\', by_month_day LONGTEXT CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_unicode_ci` COMMENT \'(DC2Type:simple_array)\', by_year_day LONGTEXT CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_unicode_ci` COMMENT \'(DC2Type:simple_array)\', by_month LONGTEXT CHARACTER SET utf8 DEFAULT NULL COLLATE `utf8_unicode_ci` COMMENT \'(DC2Type:simple_array)\', created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('CREATE TABLE service (id INT AUTO_INCREMENT NOT NULL, currency INT DEFAULT NULL, name VARCHAR(255) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, price INT NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_E19D9AD26956883F (currency), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
         $this->addSql('ALTER TABLE service ADD CONSTRAINT FK_E19D9AD26956883F FOREIGN KEY (currency) REFERENCES currency (id) ON UPDATE NO ACTION ON DELETE NO ACTION');
     }
