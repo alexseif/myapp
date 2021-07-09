c6d458dca1dc3c50a3ce8372f5267c3575855e77
Alex Seif <alex.seif@gmail.com>
2021-07-09 12:55:36 +0200

    Loc deployer update

--- loc-deployer.sh
+++ loc-deployer.sh
@@ -2,6 +2,6 @@ git pull
 composer install
 bin/console cache:clear
 bin/console doctrine:migrations:migrate --no-interaction
-bin/console doctrine:migrations:diff
+bin/console make:migration
 bin/console doctrine:migrations:migrate --no-interaction
 bin/console assets:install
