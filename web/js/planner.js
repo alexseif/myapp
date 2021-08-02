dfee83e1f22ac718f04e4d2b9d3b3ad9fdba0225
Alex Seif <alex.seif@gmail.com>
2021-08-02 12:35:54 +0200

    Initial edit on plan item

---
+++ web/js/planner.js
@@ -0,0 +1,80 @@
+/**
+ * The following content was designed & implemented under AlexSeif.com
+ **/
+
+
+let planner = {
+    init: function () {
+        this.bindEvents();
+        this.bindDraggable();
+    },
+    bindEvents: function () {
+        $('.add-item').click(this.addItem);
+    },
+    bindDraggable: function () {
+        _self = this;
+// target elements with the "draggable" class
+        interact('.draggable')
+            .draggable({
+                // enable inertial throwing
+                inertia: true,
+                // keep the element within the area of it's parent
+                modifiers: [
+                    interact.modifiers.restrictRect({
+                        restriction: 'parent',
+                        endOnly: true
+                    })
+                ],
+                // enable autoScroll
+                autoScroll: true,
+
+                listeners: {
+                    // call this function on every dragmove event
+                    move: _self.dragMoveListener,
+
+                    // call this function on every dragend event
+                    end(event) {
+                    }
+                }
+            });
+    },
+    addItem: function () {
+        $($($(this).data('template')).html())
+            .clone()
+            .appendTo('#canvas');
+        $('.edit-me').click(planner.editItem);
+        $('.save-me').click(planner.saveItem);
+    },
+    editItem: function (e) {
+        let editingItem = $(this).parents('.item');
+        editingItem.children(':not(.tools)').each(function () {
+            $(this).data('html', $(this).html());
+            $(this).html($('<input name="' + $(this).attr('class') + '" value="' + $(this).text() + '"/>'));
+        });
+    },
+    saveItem: function (e) {
+        let savedItem = $(this).parents('.item');
+        savedItem.find('input').each(function () {
+            $(this).parent().text($(this).val());
+        });
+    },
+    dragMoveListener: function (event) {
+        var target = event.target;
+        // keep the dragged position in the data-x/data-y attributes
+        var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+        var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        // translate the element
+        target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+
+        // update the posiion attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+    },
+};
+
+planner.init();
+
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = planner.dragMoveListener;
