63b58497e4557bcefac35c807af74f2716d323e7
Alex Seif <alex.seif@gmail.com>
2021-12-14 14:04:20 +0200

    scheduler draft timing

--- web/js/scheduler.js
+++ web/js/scheduler.js
@@ -5,7 +5,10 @@
 let add_minutes = function (dt, minutes) {
     return new Date(dt.getTime() + minutes * 60000);
 };
-
+let pad = function (num, size = 2) {
+    var s = "00" + num;
+    return s.substr(s.length - size);
+}
 $(function () {
     let today = new Date();
     $('.today .task-item:not(.completed)').each(function () {
@@ -13,5 +16,7 @@ $(function () {
         $est = ($est) ? $est : 60;
         today = add_minutes(today, $est);
         $(this).attr('title', today.getHours() + ":" + today.getMinutes());
+        console.log($(this).find('.task-label'));
+        $(this).find('.task-label').append(pad(today.getHours()) + ":" + pad(today.getMinutes()))
     });
 });
