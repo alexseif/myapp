145703c9aa6c6d57b3ea4bfa67f7fa3eb973c1ac
Alex Seif <alex.seif@gmail.com>
2023-01-10 12:59:06 +0200

    symfony minor upgrades

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
