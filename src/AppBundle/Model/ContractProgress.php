1a4ca84cf551bc67a2fc8ed72bdfb8bc56d59e78
Alex Seif <alex.seif@gmail.com>
2023-03-27 19:05:20 +0200

    Contract progress

--- src/AppBundle/Model/ContractProgress.php
+++ src/AppBundle/Model/ContractProgress.php
@@ -17,51 +17,81 @@ use AppBundle\Util\DateRanges;
  */
 class ContractProgress
 {
+
     /**
      * @var float
      */
     private $contractProgress;
+
     private $todaysProgress;
+
+    private $monthProgress;
 
     /**
      * @var int
      */
     private $dailyTarget;
+
     private $monthlyMinutes;
+
     private $workedMinutes;
+
     private $workingDaysSoFar;
+
     private $remainingDays;
+
     private $minutesPerDay;
+
     private $workedMinutesToday;
 
     /**
      * @var \DateTime
      */
     private $monthStart;
+
     private $monthEnd;
 
     public const DAYS_PER_MONTH = 22;
 
-    public function __construct(Contract $contract, TasksRepository $tasksRepository)
-    {
+    public function __construct(
+      Contract $contract,
+      TasksRepository $tasksRepository
+    ) {
         $this->minutesPerDay = $contract->getHoursPerDay() * 60;
         $this->monthlyMinutes = self::DAYS_PER_MONTH * $this->minutesPerDay;
 
         $this->monthStart = DateRanges::getMonthStart();
         if ($contract->getBilledOn()) {
-            $this->monthStart->setDate($this->monthStart->format('Y'), $this->monthStart->format('m'), $contract->getBilledOn());
+            $this->monthStart->setDate(
+              $this->monthStart->format('Y'),
+              $this->monthStart->format('m'),
+              $contract->getBilledOn()
+            );
         }
         $this->monthEnd = clone $this->monthStart;
         $this->monthEnd->modify('+1 month');
-        $this->workedMinutes = $tasksRepository->findDurationCompletedByClientByRange($contract->getClient(), $this->getMonthStart(), $this->getMonthEnd());
-        $this->workedMinutesToday = $tasksRepository->findCompletedByClientToday($contract->getClient());
+        $this->workedMinutes = $tasksRepository->findDurationCompletedByClientByRange(
+          $contract->getClient(),
+          $this->getMonthStart(),
+          $this->getMonthEnd()
+        );
+        $this->workedMinutesToday = $tasksRepository->findCompletedByClientToday(
+          $contract->getClient()
+        );
         $this->calcContractProgress();
 
         //@TODO: holidays and stuff
         //@TODO: WorkingDaySoFar by billing date
-        $this->workingDaysSoFar = DateRanges::numberOfWorkingDays($this->getMonthStart(), new \DateTime());
-        $this->remainingDays = (self::DAYS_PER_MONTH - $this->getWorkingDaysSoFar()) ?: 1;
-        $this->dailyTarget = ($this->getMonthlyMinutes() - $this->getWorkedMinutes()) / $this->getRemainingDays();
+        $this->workingDaysSoFar = DateRanges::numberOfWorkingDays(
+          $this->getMonthStart(),
+          new \DateTime()
+        );
+        $this->remainingDays = (self::DAYS_PER_MONTH - $this->getWorkingDaysSoFar(
+          )) ?: 1;
+        $this->monthProgress = $this->getWorkingDaysSoFar(
+          ) / self::DAYS_PER_MONTH * 100;
+        $this->dailyTarget = ($this->getMonthlyMinutes(
+            ) - $this->getWorkedMinutes()) / $this->getRemainingDays();
 
         $this->todaysProgress = $this->workedMinutesToday / $this->minutesPerDay * 100;
     }
@@ -73,7 +103,8 @@ class ContractProgress
 
     public function calcContractProgress(): void
     {
-        $this->contractProgress = $this->getWorkedMinutes() / $this->getMonthlyMinutes() * 100;
+        $this->contractProgress = $this->getWorkedMinutes(
+          ) / $this->getMonthlyMinutes() * 100;
     }
 
     public function getTodaysProgress(): float
@@ -115,4 +146,22 @@ class ContractProgress
     {
         return $this->remainingDays;
     }
+
+    /**
+     * @return float|int
+     */
+    public function getMonthProgress()
+    {
+        return $this->monthProgress;
+    }
+
+    /**
+     * @param float|int $monthProgress
+     */
+    public function setMonthProgress($monthProgress): void
+    {
+        $this->monthProgress = $monthProgress;
+    }
+
+
 }
