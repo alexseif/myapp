b463059a616da2681fe559e282bb86ba0dc8c18a
Alex Seif <alex.seif@gmail.com>
2023-03-27 12:53:05 +0200

    deployer update

--- deployer.sh
+++ deployer.sh
@@ -1,6 +1,6 @@
 git pull
 export SYMFONY_ENV=prod
 composer install --no-dev --optimize-autoloader
-php7.4 bin/console cache:clear --env=prod --no-debug
-php7.4 bin/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod
-php7.4 bin/console assets:install --env=prod
+php8.0 bin/console cache:clear --env=prod --no-debug
+php8.0 bin/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod
+php8.0 bin/console assets:install --env=prod
