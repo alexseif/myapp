40a150ecc87c7ebf20c9056bca6e7b97a37228dd
Alex Seif <alex.seif@gmail.com>
2023-11-21 10:40:10 +0200

    fixing deployer.sh

--- deployer.sh
+++ deployer.sh
@@ -1,7 +1,7 @@
 git reset HEAD --hard
 git pull
 export SYMFONY_ENV=prod
-composer install --no-dev --optimize-autoloader
+php8.2 /usr/local/bin/composer install --no-dev --optimize-autoloader
 php8.2 bin/console cache:clear --env=prod --no-debug
 php8.2 bin/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod
 php8.2 bin/console assets:install --env=prod
