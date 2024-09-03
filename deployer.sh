57fd917453fe8113cf53cde17b178868ecb68d6d
Alex Seif <alex.seif@gmail.com>
2024-09-03 13:02:36 +0300

    updating deployer to symfony 6

--- deployer.sh
+++ deployer.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
 git reset HEAD --hard
-git pull
+git pull origin master
 export SYMFONY_ENV=prod
+php8.2 /usr/local/bin/composer dump-env prod
 php8.2 /usr/local/bin/composer install --no-dev --optimize-autoloader
-php8.2 bin/console cache:clear --env=prod --no-debug
+APP_ENV=prod APP_DEBUG=0 php8.2 bin/console cache:clear --env=prod
 php8.2 bin/console doctrine:migrations:migrate --no-debug --no-interaction --env=prod
 php8.2 bin/console assets:install --env=prod
+npm install
+npm run build
--- loc-deployer.sh
+++ loc-deployer.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 git pull
-composer install
+php8.2 /usr/local/bin/composer install
 php8.2 bin/console cache:clear
 php8.2 bin/console doctrine:migrations:migrate --no-interaction
 php8.2 bin/console make:migration
 php8.2 bin/console doctrine:migrations:migrate --no-interaction
 php8.2 bin/console assets:install
+npm install
+npm run build
