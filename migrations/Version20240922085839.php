6769ed85d6e893b94f3bef2e08b6025d01effb41
Alex Seif <alex.seif@gmail.com>
2024-09-22 12:00:08 +0300

    migrations fix for production

--- migrations/Version20240922085839.php
+++ migrations/Version20240922085839.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240922084310 extends AbstractMigration
+final class Version20240922085839 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,9 +20,11 @@ final class Version20240922084310 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+
+        // Add this line to set a default value for updated_at
+        $this->addSql('UPDATE work_log SET updated_at = CURRENT_TIMESTAMP WHERE updated_at IS NULL OR updated_at = "0000-00-00 00:00:00"');
+
         $this->addSql('ALTER TABLE work_log ADD CONSTRAINT FK_F5513F598DB60186 FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE');
-        $this->addSql('UPDATE work_log SET updated_at = NOW() WHERE updated_at = "0000-00-00 00:00:00"');
-        $this->addSql('ALTER TABLE work_log MODIFY updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP');
     }
 
     public function down(Schema $schema): void
