23618c80da1944ca91a4873551956ffa372011e1
Alex Seif <alex.seif@gmail.com>
2024-09-22 11:55:34 +0300

    doctrine migration fix

--- migrations/Version20240922084310.php
+++ migrations/Version20240922084310.php
@@ -17,12 +17,13 @@ final class Version20240922084310 extends AbstractMigration
         return '';
     }
 
- public function up(Schema $schema): void
-{
-    // this up() migration is auto-generated, please modify it to your needs
-    $this->addSql('ALTER TABLE work_log ADD CONSTRAINT FK_F5513F598DB60186 FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE');
-    $this->addSql('UPDATE work_log SET updated_at = NOW() WHERE updated_at = "0000-00-00 00:00:00"');
-}
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE work_log ADD CONSTRAINT FK_F5513F598DB60186 FOREIGN KEY (task_id) REFERENCES tasks (id) ON DELETE CASCADE');
+        $this->addSql('UPDATE work_log SET updated_at = NOW() WHERE updated_at = "0000-00-00 00:00:00"');
+        $this->addSql('ALTER TABLE work_log MODIFY updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP');
+    }
 
     public function down(Schema $schema): void
     {
